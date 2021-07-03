8c60b59cd0f0ccffda97f858c24bbd89811809e7
Aditya deshmukh <adityadeshmukh7350@gmail.com>
2021-07-02 23:55:26 -0700

    server error

--- server/server.py
+++ server/server.py
@@ -1,32 +1,40 @@
-from flask import Flask, request, jsonify
-import util
+from flask import Flask, request, jsonify, render_template
+import server.util as util
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/client", static_folder='../client', template_folder="../client")
 
-@app.route('/get_location_names', methods=['GET'])
+
+@app.route('/', methods=['GET'])
+def index():
+    if request.method=="GET":
+        return render_template("app.html")
+
+
+@app.route('/get_location_names',methods=['GET'])
 def get_location_names():
-    response = jsonify({
-        'locations': util.get_location_names()
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
+    response = jsonify(
+        {
+            'locations': util.get_location_names()
+        }
+    )
+    response.headers.add('Access-Control-Allow-Origin','*')
     return response
 
-@app.route('/predict_home_price', methods=['GET', 'POST'])
+@app.route('/predict_home_price',methods=['POST'])
 def predict_home_price():
+    # we will get the data from server/user using form request
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
-    bhk = int(request.form['bhk'])
     bath = int(request.form['bath'])
+    balcony = int(request.form['balcony'])
+    Bedroom = int(request.form['Bedroom'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+        'estimated_price': util.get_etimated_prices(location, total_sqft, bath, balcony, Bedroom)
     })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
-if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
+
+if __name__ == '__main__':
+    print('Starting python flask server for Bangalore Home Price prediction..........')
     app.run()
--- server/util.py
+++ server/util.py
@@ -1,52 +1,57 @@
-import pickle
 import json
+import pickle
+import sklearn
 import numpy as np
+import os
 
+#using gloabl variables
 __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_price(location,sqft,bhk,bath):
+
+def get_etimated_prices(location, total_sqft,bath, balcony, Bedroom):
+
     try:
-        loc_index = __data_columns.index(location.lower())
+        loc_index = __data_columns.index(location.lower()) # to get column number of location, if other location then zero
     except:
         loc_index = -1
 
     x = np.zeros(len(__data_columns))
-    x[0] = sqft
+    x[0] = total_sqft
     x[1] = bath
-    x[2] = bhk
-    if loc_index>=0:
+    x[2] = balcony
+    x[3] = Bedroom
+    if loc_index >= 0:
         x[loc_index] = 1
 
-    return round(__model.predict([x])[0],2)
+    return np.round(__model.predict([x])[0][0],2)
 
-
-def load_saved_artifacts():
+def load_saved_predictors():
     print("loading saved artifacts...start")
-    global  __data_columns
+    global __data_columns
     global __locations
 
-    with open("./artifacts/columns.json", "r") as f:
+    path = os.path.dirname(__file__)
+    predictors = os.path.join(path, "predictors"),
+
+
+    with open(predictors[0] + "/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
+        __locations = __data_columns[4:]  # first 4 columns are sqft, bath, balcony, bhk
 
     global __model
     if __model is None:
-        with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
+        with open(predictors[0] + "/bangalore_home_price_model.pickle", 'rb') as f:
             __model = pickle.load(f)
     print("loading saved artifacts...done")
 
 def get_location_names():
+
     return __locations
 
 def get_data_columns():
     return __data_columns
 
-if __name__ == '__main__':
-    load_saved_artifacts()
-    print(get_location_names())
-    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
-    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
-    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
-    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
+load_saved_predictors()
+
